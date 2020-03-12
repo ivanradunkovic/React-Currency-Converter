f2178bd40036c9196bd47ddd37dac0788eb78253
Ivan <ivan1501@gmail.com>
2020-03-12 13:31:31 +0100

    Defined base currency. From and to which we will convert.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest' // this will get curre
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
-  console.log(currencyOptions)
+  // console.log(currencyOptions)  //uncoment this console log to see currency in browser when inspecting
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
 
 useEffect(() => {
   fetch(BASE_URL) //logic for fetching data from the api
     .then(res => res.json())
     .then(data => {
+      const firstCurrency = Object.keys(data.rates)[0]
       setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      setFromCurrency(data.base)
+      setToCurrency(firstCurrency)
     })
 }, []) //empty array. call effect will call only first time when I run the app
 
@@ -21,10 +26,12 @@ useEffect(() => {
     <h1>Convert</h1>
     <CurrencyRow 
     currencyOptions={currencyOptions} //added currency otionos for drop down menu
+    selectCurrency={fromCurrency}
     />
     <div className="equals">=</div>
     <CurrencyRow
     currencyOptions={currencyOptions} //added currency otionos for drop down menu
+    selectCurrency={toCurrency}
     />
     </>
   )
