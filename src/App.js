b0fe823d4af46a53433ff6777249e6c30206b37f
Ivan <ivan1501@gmail.com>
2020-03-12 13:14:47 +0100

    Curreny are shown when inspecitng app with browser. All 33 of them are shown properly.Need to add curreny options into currency row.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow'
 
-const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+const BASE_URL = 'https://api.exchangeratesapi.io/latest' // this will get current exchange rates
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
 
 useEffect(() => {
-fetch(BASE_URL)
-.then(res => res.json())
-.then(data => console.log(data))
-}, [])
+  fetch(BASE_URL) //logic for fetching data from the api
+    .then(res => res.json())
+    .then(data => {
+      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+    })
+}, []) //empty array. call effect will call only first time when I run the app
 
   return (
     <>
